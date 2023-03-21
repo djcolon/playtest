1304775cfe2cfea5847a92baa3b81b74e40f1a33
Lewis Evans <lewisevansuk@gmail.com>
2023-03-21 21:29:23 +0000

    add pytest.mark.parametrize

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,21 +1,26 @@
 """Demo test file for testing the framework."""
 
+import pytest
 from playwright.sync_api import expect
 
 from pages.bmi_page import BMIPage
 
 
-def test_bmi_metric(bmi_page: BMIPage) -> None:
+@pytest.mark.parametrize(
+    ("height", "weight", "bmi"),
+    [("180", "75", "23.15"), ("182", "80", "24.15"), ("184", "85", "25.11")],
+)
+def test_bmi_metric(bmi_page: BMIPage, height: str, weight: str, bmi: str) -> None:
     """Test function for testing the metric system calculations of the BMI calculator."""
     # Load the webpage
     bmi_page.load()
 
     # Input the height and weight
     bmi_page.select_metric_cm()
-    bmi_page.input_height_weight(height="180", weight="75")
+    bmi_page.input_height_weight(height=height, weight=weight)
 
     # Click calculate
     bmi_page.calculate_bmi()
 
     # Assert that the correct BMI is calculated and displayed
-    expect(bmi_page.bmi_result).to_have_text("23.15")
+    expect(bmi_page.bmi_result).to_have_text(bmi)
