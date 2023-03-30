6b40ff730746c7ef7abe70e7713cf238c3093714
Lewis Evans <lewisevansuk@gmail.com>
2023-03-30 16:33:01 +0100

    added custom report plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,55 +2,103 @@
 
 import datetime
 import json
+import time
 from pathlib import Path
+from typing import Self
 
 import pytest
+from _pytest.terminal import TerminalReporter
 from playwright.sync_api import Page
 
 from pages.bmi_page import BMIPage
 
 
+# Page fixtures
 @pytest.fixture()
 def bmi_page(page: Page) -> BMIPage:
     """Pytest fixture to create instance of the BMIPage class."""
     return BMIPage(page)
 
 
-def pytest_sessionstart(session: pytest.Session):
-    session.results = []
+# Hooks
+def pytest_addoption(parser: pytest.Parser) -> None:
+    """Add a command line option."""
+    parser.addoption(
+        "--playtest-report",
+        action="store_true",
+        default=None,
+        help="Generate a json test report.",
+    )
 
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item: pytest.Item, call: pytest.CallInfo):
-    outcome = yield
-    result = outcome.get_result()
+def pytest_configure(config: pytest.Config) -> None:
+    """Configure the playtest-report plugin."""
+    playtest_report = config.option.playtest_report
+    if playtest_report and not hasattr(config, "workerinput"):
+        config._playtest_report_plugin = PlaytestReportPlugin(config)
+        config.pluginmanager.register(config._playtest_report_plugin)
 
-    if result.when == "call":
-        report = {
-            "nodeid": result.nodeid,
-            "when": result.when,
-            "outcome": result.outcome,
-            "duration": call.duration,
+
+def pytest_unconfigure(config: pytest.Config):
+    reporter: TerminalReporter = config.pluginmanager.get_plugin("terminalreporter")
+    duration = time.time() - reporter._sessionstarttime
+    reporter.write_sep(
+        "=", "duration: {} seconds".format(duration), yellow=True, bold=True
+    )
+
+
+class PlaytestReportPlugin:
+    """Class containing logic for using pytest hooks to create a json report file."""
+
+    def __init__(self: Self, config: pytest.Config) -> None:
+        """Initialise the object with a unique report file path."""
+        self._config = config
+        self._report_path = Path(
+            f"./reports/{str(datetime.datetime.now()).replace(' ', '-')}.json"
+        )
+        self._metadata: list = []
+        self._collect_data: list = []
+        self._test_data: list = []
+        self._total_duration: float = 0
+
+    def pytest_collectreport(self: Self, report: pytest.CollectReport) -> None:
+        """Get details of the tests that have been collected."""
+        data = self._config.hook.pytest_report_to_serializable(
+            config=self._config, report=report
+        )
+        self._collect_data.append(data)
+
+    def pytest_runtest_logreport(self: Self, report: pytest.TestReport) -> None:
+        """Get details of the tests that have been run."""
+        data = self._config.hook.pytest_report_to_serializable(
+            config=self._config, report=report
+        )
+        self._test_data.append(data)
+
+    def pytest_sessionfinish(self: Self, exitstatus: int) -> None:
+        """Generate report at the end of the pytest session."""
+        # Get the metadata for the report
+        data = {"pytest_version": pytest.__version__, "exitstatus": exitstatus}
+        self._metadata.append(data)
+
+        # Calculate total duration of test execution
+        for x in self._test_data:
+            self._total_duration += x["duration"]
+        self._metadata.append({"total_duration": round(self._total_duration, 2)})
+
+        # Create a dict from all the lists of data
+        json_data = {
+            "metadata": self._metadata,
+            "collect_data": self._collect_data,
+            "test_data": self._test_data,
         }
 
-        if call.excinfo is not None:
-            report.update(
-                {
-                    "errors": {
-                        "exception_type": call.excinfo.typename,
-                        "exception_message_formatted": call.excinfo.exconly(
-                            tryshort=True
-                        ),
-                        "exception_traceback": str(call.excinfo.traceback.__str__()),
-                    }
-                }
-            )
+        # Dump the dictionary as json to the json file
+        with open(self._report_path, "x") as f:
+            json.dump(json_data, f)
 
-        item.session.results.append(report)
-
-
-def pytest_sessionfinish(session: pytest.Session):
-    with open(
-        Path(f"./reports/{str(datetime.datetime.now()).replace(' ', '-')}.json"), "x"
-    ) as f:
-        json.dump(session.results, f)
+    def pytest_terminal_summary(self: Self, terminalreporter: TerminalReporter) -> None:
+        """Write details of the test report to the terminal."""
+        terminalreporter.write_sep(
+            "-", f"generated report log file: {self._report_path}"
+        )
