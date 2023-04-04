db3448f1b90a93fc8d3611800542c699945ff933
Lewis Evans <lewisevansuk@gmail.com>
2023-04-04 16:26:08 +0100

    added utiliies to generate list of cli arguments

---
+++ utils/cli_args.py
@@ -0,0 +1,18 @@
+"""Functions to produce the cli arguments for pytest."""
+
+
+def generate_cli_args(config: dict) -> list:
+    """Return a list of string arguments for the pytest.main() call."""
+    cli_args = []
+
+    if config["verbose"]:
+        cli_args.append("-v")
+
+    if config["marks"] is not None:
+        cli_args.append("-m")
+        cli_args.append(" or ".join([str(m) for m in config["marks"]]))
+
+    if config["playtest-report"]:
+        cli_args.append("--playtest-report")
+
+    return cli_args
