2526f75ab9e2ffcb8188ac57213179b28201014f
Lewis Evans <lewisevansuk@gmail.com>
2023-04-12 12:29:34 +0100

    remove self type annotations

--- pages/bmi_page.py
+++ pages/bmi_page.py
@@ -1,6 +1,5 @@
 """File for defining the class for the BMI page."""
 
-from typing import Self
 
 from playwright.sync_api import Locator, Page
 
@@ -10,7 +9,7 @@ class BMIPage:
 
     URL: str = "https://patient.info/doctor/bmi-calculator-calculator"
 
-    def __init__(self: Self, page: Page) -> None:
+    def __init__(self, page: Page) -> None:
         """Initialise the page object and it's defined locators."""
         self.page: Page = page
         self.cookies_agree_btn: Locator = page.get_by_role("button", name="AGREE")
@@ -22,25 +21,25 @@ class BMIPage:
         self.calculate_btn: Locator = page.get_by_role("button", name="Calculate")
         self.bmi_result: Locator = page.locator(".bmi-result")
 
-    def load(self: Self) -> None:
+    def load(self) -> None:
         """Load the website."""
         self.page.goto(self.URL)
         self.cookies_agree_btn.click()
 
-    def select_metric(self: Self) -> None:
+    def select_metric(self) -> None:
         """Check the metric radio button."""
         self.metric_radio_btn.check()
 
-    def select_metric_cm(self: Self) -> None:
+    def select_metric_cm(self) -> None:
         """Check the metric radio button and select the centimetres option."""
         self.metric_radio_btn.check()
         self.height_options.select_option(self.height_option_cm)
 
-    def input_height_weight(self: Self, height: str, weight: str) -> None:
+    def input_height_weight(self, height: str, weight: str) -> None:
         """Input the height and weight values."""
         self.height_input.fill(height)
         self.weight_input.fill(weight)
 
-    def calculate_bmi(self: Self) -> None:
+    def calculate_bmi(self) -> None:
         """Click the calculate button."""
         self.calculate_btn.click()
