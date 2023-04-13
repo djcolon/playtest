779827fa0479a44c59f34ef8c22d8702219859b0
Lewis Evans <lewisevansuk@gmail.com>
2023-04-13 14:18:17 +0100

    test file and directory cli options

--- utils/cli_args.py
+++ utils/cli_args.py
@@ -8,6 +8,12 @@ def generate_cli_args(config: dict) -> list:
     if config["verbose"]:
         cli_args.append("-v")
 
+    if config["test_dir"] is not None:
+        cli_args.append(config["test_dir"])
+
+    if config["test_file"] is not None:
+        cli_args.append(config["test_file"])
+
     if config["marks"] is not None:
         cli_args.append("-m")
         cli_args.append(" or ".join([str(m) for m in config["marks"]]))
