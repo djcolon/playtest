39da8dbb8d7d27979992335a1b3d38b6bf4fc5e8
Lewis Evans <lewisevansuk@gmail.com>
2023-05-02 16:00:43 +0100

    add timestamped path to share between plugins

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Main file for running Playtest."""
 
+from datetime import datetime
+
 import pytest
 
 from utils.cli_args import generate_cli_args
@@ -7,6 +9,8 @@ from utils.load_config import load_yaml_config
 
 if __name__ == "__main__":
     config = load_yaml_config()
-    cli_args = generate_cli_args(config=config)
+    path_timestamp = f"./reports/{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}"
+
+    cli_args = generate_cli_args(config=config, path=path_timestamp)
 
     pytest.main(args=cli_args)
