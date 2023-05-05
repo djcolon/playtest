de3dff3f93d90457f4aeaf348ddbea13595ce985
Lewis Evans <lewisevansuk@gmail.com>
2023-05-05 11:46:23 +0100

    added app.py file for streamlit frontend

--- web/app.py
+++ web/app.py
@@ -1,17 +1,34 @@
 """Main file for running the Streamlit frontend."""
 
+import sys
+from datetime import datetime
+from pathlib import Path
+
 import streamlit as st
-from components import run
 
-st.set_page_config(page_title="Playtest", page_icon="random")
-st.title("Playtest")
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from components import run, run_config
 
+from utils.cli_args import generate_cli_args
 
-with st.sidebar:
-    run_btn = st.button(label="Run", type="primary")
+if __name__ == "__main__":
+    st.set_page_config(page_title="Playtest", page_icon="random")
+    st.title("Playtest")
 
-if run_btn:
-    while True:
-        run_playtest = run()
-        if run_playtest is not None:
-            break
+    with st.sidebar:
+        run_btn = st.button(label="Run", type="primary")
+
+    if run_btn:
+        config = run_config()
+        now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+
+        path = Path.cwd() / "reports" / now
+
+        path.mkdir()
+
+        cli_args = generate_cli_args(config=config, path=str(path))
+
+        while True:
+            run_playtest = run(cli_args=cli_args)
+            if run_playtest is not None:
+                break
