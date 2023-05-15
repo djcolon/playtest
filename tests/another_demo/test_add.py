13ae389748ce347ed6d5e9167836036b73b54551
Lewis Evans <lewisevansuk@gmail.com>
2023-05-15 16:38:35 +0100

    add more test examples

---
+++ tests/another_demo/test_add.py
@@ -0,0 +1,11 @@
+"""Demo test file for testing the framework with multiple test subdirectories."""
+
+
+def test_add_one() -> None:
+    """Simple add function for demonstration."""
+    assert 3 + 1 == 4
+
+
+def test_add_two() -> None:
+    """Simple add function for demonstration."""
+    assert 3 + 2 == 5
---
+++ tests/another_demo/test_subtract.py
@@ -0,0 +1,11 @@
+"""Demo test file for testing the framework with multiple test subdirectories."""
+
+
+def test_subtract_one() -> None:
+    """Simple subtract function for demonstration."""
+    assert 3 - 1 == 2
+
+
+def test_subtract_two() -> None:
+    """Simple subtract function for demonstration."""
+    assert 3 - 2 == 1
